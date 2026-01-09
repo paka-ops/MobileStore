781cfdbbd021a09f3e2d5d78c0c5f46e90de50ad
paka-ops <pakawess@gmail.com>
2026-01-09 02:46:54 +0000

    first commit

---
+++ src/test/java/com/example/egestion/EGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
